12f08127b0f0f35e780bc7bd1d07fa8528a43ffe
harishshete <42139616+harishshete@users.noreply.github.com>
2025-05-27 16:02:45 +0530

    Update status.js
    Updated status API

--- Routes/status.js
+++ Routes/status.js
@@ -4,9 +4,21 @@ const ordersController = require('../Controller/OrsersController/ordersControlle
 
 
 router.get('/', (req, res) => {
+    const sizeInBytes = 3.7 * 1024 * 1024; // 3.5 MB
+  // Create a string of repeated 'a' characters to match the size
+  const responseData = 'a'.repeat(sizeInBytes);
+
+    res.status(200).json(
+    responseData
+        //    message : "You are not suthorized to sccess this resource"
+    )
+});
+
+/*
+router.get('/', (req, res) => {
     res.status(403).json({
         message : "You are not suthorized to sccess this resource"
     })
 });
-
+*/
 module.exports = router;
